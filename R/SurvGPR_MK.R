073b83dada2deff70b88d0e58d3ee9d90c808414
Aaron J. Molstad <amolstad@fredhutch.org>
2018-10-03 22:24:57 -0700

    Update SurvGPR_MK.R

--- R/SurvGPR_MK.R
+++ R/SurvGPR_MK.R
@@ -274,7 +274,7 @@ SurvGPR_MK = function(time, status, Z, K, tol,
   cond.mean <-  Z1%*%beta.temp + Kbeta%*%(Y0 - Z0%*%beta.temp)
   cond.Var <- Omega.temp[train.cen, train.cen] - t(solve(Omega.temp[train.obs, train.obs], Omega.temp[train.obs, train.cen]))%*%Omega.temp[train.obs, train.cen]
   d <- pmvnorm(lower=Y1, upper=rep(Inf, length(Y1)), mean=c(cond.mean),
-        sigma=cond.Var)$error
+        sigma=cond.Var)[1]
   loglik <- log(d) -.5*determinant(Omega.temp[train.obs, train.obs], logarithm=TRUE)$modulus[1] 
   - .5*t(Y0 - Z0%*%beta.temp)%*%solve(Omega.temp[train.obs,train.obs], Y0 - Z0%*%beta.temp) - .5*length(Y0)*log(2*pi)
   AIC <- 2*(dim(Z)[2] + dim(K)[3]) - 2*loglik
